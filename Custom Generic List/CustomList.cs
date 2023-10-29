3afb19244f8637362ddc2b28b3541cc46778455a
Burhanaddinzm <burhanaddinzm@code.edu.az>
2023-10-29 19:39:55 +0400

    Custom Generic List

---
+++ Custom Generic List/CustomList.cs
@@ -0,0 +1,24 @@
+﻿    
+namespace Custom_GenericList
+{
+    public class CustomList<T>  
+    {
+        T[] objects = new T[10];
+        public void Add(T obj)
+        {
+            if (objects[objects.Length - 1 ] != null)
+            {
+                Array.Resize(ref objects, objects.Length + 10);
+            }
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i] = obj;
+                break;
+            }
+        }
+        public T[] Get()
+        {
+            return objects;
+        }
+    }
+}
---
+++ Custom Generic List/Program.cs
@@ -0,0 +1,14 @@
+﻿using Custom_GenericList;
+
+CustomList<int> ints = new CustomList<int>();
+
+ints.Add(1);
+ints.Add(2);
+ints.Add(3);
+ints.Get();
+
+CustomList<string> strings = new CustomList<string>();
+strings.Add("1");
+strings.Add("2");
+strings.Add("3");
+strings.Get();
